5d3232387a642b0bd90cd596f39e906cd7c38e68
krau <71133316+krau@users.noreply.github.com>
2024-09-22 19:44:18 +0800

    refactor: remove unuse import style

--- components/ArtalkClient.vue
+++ components/ArtalkClient.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { useArtalkWithOptions } from "../client";
 import 'artalk/dist/Artalk.css'
-import '../client/styles/index.scss'
 
 useArtalkWithOptions();
 </script>
